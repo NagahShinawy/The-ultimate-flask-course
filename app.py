df3e027c14182fc4e97eec7d6562568519782b15
nagah <nagah.shinawy@gmail.com>
2022-10-16 10:41:22 +0200

    health

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 PRODUCTS = [
-        {"id": 1, "title": "Labtop", "price": 234.89},
-        {"id": 2, "title": "mobile", "price": 968.89},
-        {"id": 3, "title": "coffee machine", "price": 3213.89},
-        {"id": 4, "title": "bed", "price": 32.89},
-    ]
+    {"id": 1, "title": "Labtop", "price": 234.89},
+    {"id": 2, "title": "mobile", "price": 968.89},
+    {"id": 3, "title": "coffee machine", "price": 3213.89},
+    {"id": 4, "title": "bed", "price": 32.89},
+]
 
-NO_PRODUCTS = 'No Product(s)'
+NO_PRODUCTS = "No Product(s)"
 
 
 @app.route("/")
@@ -29,5 +29,10 @@ def get_product(_id):
     return NO_PRODUCTS
 
 
-if __name__ == '__main__':
+@app.route("/health")
+def health_check():
+    return "<h3>Health is Ok</h3>"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
