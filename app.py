b26ebb9e3726d027736b94f81ed3f0d7bccdc4ff
nagah <nagah.shinawy@gmail.com>
2022-10-16 10:37:33 +0200

    get products & single product

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+PRODUCTS = [
+        {"id": 1, "title": "Labtop", "price": 234.89},
+        {"id": 2, "title": "mobile", "price": 968.89},
+        {"id": 3, "title": "coffee machine", "price": 3213.89},
+        {"id": 4, "title": "bed", "price": 32.89},
+    ]
+
+NO_PRODUCTS = 'No Product(s)'
 
 
 @app.route("/")
@@ -10,12 +18,15 @@ def index():
 
 @app.route("/products")
 def products():
-    return [
-        {"id": 1, "title": "Labtop", "price": 234.89},
-        {"id": 2, "title": "mobile", "price": 968.89},
-        {"id": 3, "title": "coffee machine", "price": 3213.89},
-        {"id": 4, "title": "bed", "price": 32.89},
-    ]
+    return PRODUCTS
+
+
+@app.route("/products/<int:_id>")
+def get_product(_id):
+    for product in PRODUCTS:
+        if product.get("id") == _id:
+            return product
+    return NO_PRODUCTS
 
 
 if __name__ == '__main__':
